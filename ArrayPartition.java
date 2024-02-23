e2f8050bfdf65c31c2ca4f88588bedef83282ad7
Gustavo <gustavosbortolon@gmail.com>
2024-02-23 12:23:56 -0300

    Update ArrayPartition

--- ArrayPartition.java
+++ ArrayPartition.java
@@ -3,10 +3,12 @@ public class ArrayPartition {
     
 
     public static void main(String[] args) {
-        
+        int[] lista = {5,2,1,3};
+        int result = arrayPairSum(lista);
+        System.out.println(result);
     }
 
-    public int arrayPairSum(int[] nums){
+    public static int arrayPairSum(int[] nums){
         Arrays.sort(nums);
         int pairSum = 0;
         for(int i = 0; i < nums.length; i += 2){
